ad98c404a3b2d88946ad27afd5889185a5228c4b
raghavan <raghavanmarch2002@gmail.com>
2022-10-17 17:09:24 +0530

    fix marketplace bug

--- src/nord.js
+++ src/nord.js
@@ -125,7 +125,7 @@ async function initNord() {
 
     ////////////////////////////////////// Preprocessor ///////////////////////////////////////////
 
-    if (CONFIG.dev && CONFIG.localCSS) {
+    if (CONFIG.dev && CONFIG.localCSS && !isMarketplace) {
         server = "";
     } else {
         if (isMarketplace) {
